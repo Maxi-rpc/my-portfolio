39fbdcceef64927d48469cb4e5698ea7d0a60826
Maximiliano <maxirpc2607@gmail.com>
2021-08-27 17:38:52 -0300

    Update
    Se crea script para fecha actual, head-content dinámico
    Se modifica index, con tabs en sidebar, se ingresa primeros maquetados de home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 console.log("Hola Mundo DOM");
+
+// mostrar fecha actual
+var fecha = document.getElementById("fecha");
+var hoy = new Date();
+fecha.innerText = hoy.getFullYear();
+
+//head-content dinamico
+var head = document.getElementById("head-content");
+var tab = document.getElementById("myTab");
+
+tab.addEventListener("click", function (e) {
+  var tabName = e.target.name;
+  if (tabName != null) {
+    head.innerText = tabName;
+  }
+});
